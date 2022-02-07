eb0e66f71ae0e88dbbffdb6ae34efe0fb2a3719f
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-08 01:55:56 +0900

    :memo: #2.5 | Arrays

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const amIFat = null;
-let something;
-console.log(something, amIFat);
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from array
+console.log(daysOfWeek, daysOfWeek[4]);
+
+// Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
