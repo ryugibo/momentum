7c11736fdd4d35bd2466be7db2e36aaa1a1003cb
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-08 01:38:12 +0900

    :memo: #2.4 | Boolean
    - boolean: true/false
    - null: 값이 비어있음
      let some = null;
    - undefined: 값이 정해져있지 않음
      let some;

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const a = 5;
-const b = 2;
-let myName = "gibo";
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "ryugibo";
-
-console.log("your name is " + myName);
+const amIFat = null;
+let something;
+console.log(something, amIFat);
