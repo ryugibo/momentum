5d591ea10e7bf3b91c45a7033013803d9105a918
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-08 20:20:37 +0900

    :memo: #2.6 | Objects

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
-
-// Get Item from array
-console.log(daysOfWeek, daysOfWeek[4]);
-
-// Add one more day to the array
-daysOfWeek.push("sun");
-
-console.log(daysOfWeek);
+const player = {
+  name: "gibo",
+  points: 10,
+  fat: true,
+};
+console.log(player);
+player.lastname = "potato";
+player.points = player.points + 55;
+console.log(player.points);
