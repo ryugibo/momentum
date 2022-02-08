06cda33f7d74da3c579cacaba1d705442aa1973d
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-08 21:39:41 +0900

    :memo: #2.13 | Conditionals

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-const calculator = {
-  plus: function(l, r) {
-    return l + r;
-  },
-  minus: function(l, r) {
-    return l - r;
-  },
-  times: function(l, r) {
-    return l * r;
-  },
-  divide: function(l, r) {
-    return l / r;
-  },
-  power: function(l, r) {
-    return l ** r;
-  }
-}
+const age = parseInt(prompt("How old are you?"));
 
-const plusResult = calculator.plus(1, 2);
-const minusResult = calculator.minus(plusResult, 10);
-const timesResult = calculator.times(4, minusResult);
-const divideResult = calculator.divide(timesResult, plusResult);
-const powerResult = calculator.power(divideResult, minusResult);
+console.log(age, typeof age);
