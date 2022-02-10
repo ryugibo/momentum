4a7cc95da34cf064245d1ff630d67e596ffc9da1
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-10 22:46:24 +0900

    :memo: #4.1 | Form Submission

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function handleLoginButtonClick() {
-  console.log("hello", loginInput.value);
+  const username = loginInput.value;
+  console.log("hello", username);
 }
 
-loginButton.addEventListener("click", handleLoginButtonClick);
+// loginButton.addEventListener("click", handleLoginButtonClick);
