ff323c81c650dbdc50e88f45c60ee70bc10d9911
ryugibo <7928632+ryugibo@users.noreply.github.com>
2022-02-12 23:58:02 +0900

    :memo: #8.1 | Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,19 @@
+const API_KEY = "b9f77474d3fc53be4b542a6cab77b8fa";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  
-  console.log(position);
-  console.log(lat);
-  console.log(lng);
+  const lon = position.coords.longitude;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const weather = document.querySelector("#weather span:first-child");
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+
+    const city = document.querySelector("#weather span:last-child");
+    city.innerText = data.name;
+  });
 }
 
 function onGeoError() {
